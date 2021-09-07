15028032cd3268517c0f25f4f5c7829140e99581
j-dolenc <jurij1.dolenc@gmail.com>
2021-09-07 15:02:34 +0200

    osnovno drevo narisano.

---
+++ src/components/trash/ProjectStructV1.tsx
@@ -0,0 +1,26 @@
+import React, { Fragment, useState } from "react";
+import classes from "./ProjectStruct.module.css";
+import 'react-sortable-tree/style.css';
+import { SortableTreeWithoutDndContext as SortableTree, TreeItem } from 'react-sortable-tree';
+const ProjectStruct = () => {
+    const treeData=[
+        { title: 'Chicken', children: [{ title: 'Egg' }] },
+        { title: 'Fish', children: [{ title: 'fingerline' }] },
+      ];
+    // const [treeData,setTreeData] = useState([
+    //     { title: 'Chicken', children: [{ title: 'Egg' }] },
+    //     { title: 'Fish', children: [{ title: 'fingerline' }] },
+    //   ]);
+    const onChangeHandler = (treeData: TreeItem[]) => {
+        console.log(treeData);
+        //setTreeData();
+    }
+  return (
+    <div>
+      <p>kuku</p>
+      <SortableTree  treeData ={treeData}
+          onChange={onChangeHandler} className={classes.hey}></SortableTree>
+    </div>
+  );
+};
+export default ProjectStruct;
--- src/pages/Projekt.tsx
+++ src/pages/Projekt.tsx
@@ -1,17 +1,22 @@
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import ProjectStruct from "../components/projects/ProjectStruct";
 import classes from "./Projekt.module.css";
 
 const Projekt = () => {
   return (
-      <div className={classes.project}>
-        {/* <p>Ime projekta</p>  */}
-        <div className={classes.box}>
-          <p>Tukaj bo prikazan projekt...Nova komponenta</p>
-        </div>
-        <div className={classes.options}>
-            <p>Možnosti komponenta</p>
-        </div>
+    <div className={classes.project}>
+      {/* <p>Ime projekta</p>  */}
+      <div className={classes.box}>
+        {/* <p>Tukaj bo prikazan projekt...Nova komponenta</p> */}
+        <DndProvider backend={HTML5Backend}>
+          <ProjectStruct />
+        </DndProvider>
       </div>
-    
+      <div className={classes.options}>
+        <p>Možnosti komponenta</p>
+      </div>
+    </div>
   );
 };
 export default Projekt;
