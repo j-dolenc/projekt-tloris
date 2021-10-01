7c2c850d079f57a7e19619235d9b716bff62edf4
j-dolenc <jurij1.dolenc@gmail.com>
2021-10-01 13:07:59 +0200

    Podatki v dropdown preko props.

--- client/src/components/UI/DropDown.tsx
+++ client/src/components/UI/DropDown.tsx
@@ -1,46 +1,14 @@
 import Select from 'react-select'
-
-const DropDown = () => {
+import classes from './DropDown.module.css';
+const DropDown: React.FC <{data:{value:string,label:string}[]}> = (props) => {
+	//uporabi propse da dobis option zato da jih 
+	//lazje uporbis pol v addProject komponenti
 	
-	const options = [
-		{ value: 'chocolate', label: 'Chocolate' },
-		{ value: 'strawberry', label: 'Strawberry' },
-		{ value: 'vanilla', label: 'Vanilla' }
-	  ];
-	  let data:{value:string,label:string}[]=[];
-	  const getOptions =async() => {
-		try {
-            
-            const response = await fetch(`http://192.168.38.164:5000/users`);//?pass=${enteredPassword}
-            const jsonData = await response.json();
-            console.log(jsonData);
-			for(let i = 0; i < jsonData.length;i++){
-				//TODO: dodaj vse kar dovis v usestate options, nato pa se oddelke
-				data.push({value:jsonData[i].id,label:jsonData[i].username});
-			}
-			console.log(data);
-        } catch (error:any) {
-            console.error(error.message);
-        }
-		try {
-            
-            const response = await fetch(`http://192.168.38.164:5000/oddelki`);//?pass=${enteredPassword}
-            const jsonData = await response.json();
-            console.log(jsonData);
-			for(let i = 0; i < jsonData.length;i++){
-				//TODO: dodaj vse kar dovis v usestate options, nato pa se oddelke
-				data.push({value:`od${jsonData[i].id}`,label:jsonData[i].ime});
-			}
-			console.log(data);
-        } catch (error:any) {
-            console.error(error.message);
-        }
-
-	  }
-	  getOptions();
-	return <Select
+	  
+	return <Select className={classes.drop}
 			closeMenuOnSelect={false}
       		isMulti
-      		options={data} />;
+      		options={props.data} />;
+
 }
 export default DropDown;
--- client/src/components/projects/AddProject.tsx
+++ client/src/components/projects/AddProject.tsx
@@ -4,11 +4,41 @@ import classes from "./AddProject.module.css";
 
 const AddProject: React.FC<{ onCancelClick: () => void }> = () => {
   const onSubmitHandler = async (event: React.FormEvent) => {};
-  
+  let data:{value:string,label:string}[]=[];
+	  const getOptions =async() => {
+		try {
+            
+            const response = await fetch(`http://192.168.38.164:5000/users`);//?pass=${enteredPassword}
+            const jsonData = await response.json();
+            console.log(jsonData);
+			for(let i = 0; i < jsonData.length;i++){
+				//TODO: dodaj vse kar dovis v usestate options, nato pa se oddelke
+				data.push({value:jsonData[i].id,label:jsonData[i].username});
+			}
+			console.log(data);
+        } catch (error:any) {
+            console.error(error.message);
+        }
+		try {
+            
+            const response = await fetch(`http://192.168.38.164:5000/oddelki`);//?pass=${enteredPassword}
+            const jsonData = await response.json();
+            console.log(jsonData);
+			for(let i = 0; i < jsonData.length;i++){
+				//TODO: dodaj vse kar dovis v usestate options, nato pa se oddelke
+				data.push({value:`od${jsonData[i].id}`,label:jsonData[i].ime});
+			}
+			console.log(data);
+        } catch (error:any) {
+            console.error(error.message);
+        }
+
+	  }
+	  getOptions();
   
   return (
     <section className={classes.auth}>
-      <h1>Add a New Project</h1>
+      <h1>Dodaj Nov Projekt</h1>
       <form onSubmit={onSubmitHandler}>
         <div className={classes.control}>
           <label htmlFor="projectName">Ime Projekta</label>
@@ -22,9 +52,12 @@ const AddProject: React.FC<{ onCancelClick: () => void }> = () => {
           <label htmlFor="description">Opis Projekta</label>
           <input type="text" id="description"></input>
         </div>
+        <div className={classes.control}>
+          <label>Lahko Ga Vidijo</label>
+          <DropDown data={data}/>
+        </div>
         {/* moznost, kdo lahko vidi projekt, naredi
             z izbiro večih moćnosti.... svoja komponenta... */}
-        <DropDown/>
         
       </form>
     </section>
